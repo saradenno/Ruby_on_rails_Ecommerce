16bf482b7aa55f8cee75ee420f9973bd5092776d
Sara Denno <sarahdenno2005@gmail.com>
2024-10-29 14:57:11 +0100

    Destory methode geimplimenteerd in cartscontroller , De destroy-methode zoekteen item op basis van product_id met behulp van find_by_product_id.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -39,6 +39,19 @@ class CartsController < ApplicationController
     end
 
 
+    def destroy
+        @cart_item= @current_cart.cart_items.find_by_product_id(@product.id)
+        @cart_item.destroy
+        redirect_to cart_path(@current_cart)
+    end
+
+
+   
+      
+
+
+  
+
     private
     def set_product
         @product = Product.find(params[:product_id])
