a6af3a82862f12eed633d0fa64925927134f6986
Sara Denno <sarahdenno2005@gmail.com>
2024-10-29 15:02:29 +0100

    De stripe_session-methode in cartscontroller , initialiseert een Stripe Checkout-sessie met winkelwageninhoud, inclusief hoeveelheid en prijs per item. Verzendregio’s worden beperkt via allowed_countries. De clientSecret wordt geretourneerd voor veilige sessie-verificatie

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -46,9 +46,33 @@ class CartsController < ApplicationController
     end
 
 
-   
-      
 
+    def stripe_session
+        session = Stripe::Checkout::Session.create({
+        ui_mode: 'embedded',
+        line_items: @current_cart.cart_items.map do |cart_item|{
+            # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+            price_data: {
+              currency: "usd",
+              unit_amount: (cart_item.product.price * 100).to_i,
+              product_data: {
+                name: @current_cart.products.map(&:name).join(", "),
+              },
+            },
+            quantity: cart_item.quantity,
+          }
+        end,
+
+          shipping_address_collection:{
+            allowed_countries: STRIPE_SUPPORTED_COUNTRIES
+          },
+          
+          mode: 'payment',
+          return_url: success_cart_url(@current_cart.secret_id),
+      })
+  
+      render json: { clientSecret: session.client_secret } 
+    end 
 
   
 
