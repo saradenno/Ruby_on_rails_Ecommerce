4d4f56ce3455e0512e64ff29f77c50c57178bb00
Sara Denno <sarahdenno2005@gmail.com>
2024-10-29 14:20:55 +0100

    het model Cart beschrijft  de relaties (has_many voor cart_items en products via cart_items).     Voegt een unieke secret_id toe met een before_create callback.     enum :status definieert pending en complete staten

---
+++ app/models/cart.rb
@@ -0,0 +1,17 @@
+class Cart < ApplicationRecord
+    belongs_to :user , optional: true
+    has_many :cart_items, dependent: :destroy
+    has_many :products, through: :cart_items
+
+    before_create :set_secret_id
+
+
+    enum :status, ["pending", "complete"]
+
+
+    private 
+    def set_secret_id
+        self.secret_id = SecureRandom.uuid + DateTime.now.to_i.to_s
+    end
+
+end
