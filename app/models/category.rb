af24a46563a0debd9d5357f732be8781cc68f849
Sara Denno <sarahdenno2005@gmail.com>
2024-10-30 17:19:25 +0100

    in de model catgory , heb ik validaties toegevoegd voor foutafhandeling - Zorgt ervoor dat `name` en `description` verplicht zijn

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,7 @@ class Category < ApplicationRecord
   has_many :products
   has_one_attached :image
   has_rich_text :description
+
+  validates :name, presence: true
+  validates :description, presence: true
 end
