2d1eb9ed73282ec308e8e8ce788f98809f52e534
Sara Denno <sarahdenno2005@gmail.com>
2024-10-30 17:38:41 +0100

    Hier voeg ik validaties toe aan Category-model voor veldinvoer: - `name` moet ten minste één letter bevatten. - `description` moet aanwezig zijn en mag niet alleen uit nummers bestaan. - aangepaste validatie om invoer die alleen nummers bevat te beperken.

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,14 @@ class Category < ApplicationRecord
   has_one_attached :image
   has_rich_text :description
 
-  validates :name, presence: true
+  validates :name, presence: true, format: { with: /[a-zA-Z]/, message: "must include at least one letter" }
   validates :description, presence: true
+  validate :description_cannot_be_only_numbers
+
+  #custom validation
+  def description_cannot_be_only_numbers
+    if description.present? && description.to_plain_text.match?(/\A[0-9]+\z/)
+      errors.add(:description, "cannot be only numbers")
+    end
+  end
 end
