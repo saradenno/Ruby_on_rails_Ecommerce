3dbcb745d9035e041837322799eb57eefbfed77c
Sara Denno <sarahdenno2005@gmail.com>
2024-10-30 17:20:11 +0100

    In de model Product heb ik validaties toegevoegd aan l voor foutafhandeling
    - Stelt `name`, `price` en `description` verplicht in Product-model.

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,8 @@ class Product < ApplicationRecord
   belongs_to :category, optional: true    
   has_many :cart_items
   # to make sure it doesnt have to have a category
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
