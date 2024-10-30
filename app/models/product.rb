2287b312fb14f44909e568f010e066f6bdc6e889
Sara Denno <sarahdenno2005@gmail.com>
2024-10-30 17:34:21 +0100

    Hier voeg ik  aangepaste validatie toe voor omschrijving in Product-model. Zorgt ervoor dat omschrijving niet alleen cijfers bevat door to_plain_text te gebruiken om rich text om te zetten naar eenvoudige tekst voor validatie.

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,15 @@ class Product < ApplicationRecord
   has_many :cart_items
   # to make sure it doesnt have to have a category
 
-  validates :name, presence: true
+  validates :name, presence: true, format: { with: /[a-zA-Z]/, message: "must include at least one letter" }
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
+  validate :description_cannot_be_only_numbers
+
+  #custom validation
+  def description_cannot_be_only_numbers
+    if description.present? && description.to_plain_text.match?(/\A[0-9]+\z/)
+      errors.add(:description, "cannot be only numbers")
+    end
+  end
 end
